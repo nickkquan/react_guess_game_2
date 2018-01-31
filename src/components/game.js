3b96a81de65461441c4faacbccdf22b6beba8741
Nick Quan <nickkquan@gmail.com>
2018-01-31 14:40:47 -0800

    completed adding user input

--- src/components/game.js
+++ src/components/game.js
@@ -1,41 +1,80 @@
 import React, { Component } from "react";
 
 class Game extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      randomNumber: 0
-    };
-  }
+		this.state = {
+			randomNumber: 0,
+			userNumber: ""
+		};
+	}
 
-  componentDidMount() {
-    this.generateRandomNumber();
-  }
+	componentDidMount() {
+		this.generateRandomNumber();
+	}
 
-  generateRandomNumber() {
-    const randNum = Math.floor(Math.random() * 10) + 1;
+	generateRandomNumber() {
+		const randNum = Math.floor(Math.random() * 10) + 1;
 
-    this.setState({
-      randomNumber: randNum
-    });
-  }
+		this.setState({
+			randomNumber: randNum
+		});
+	}
 
-  resetGame() {
-    this.generateRandomNumber();
-  }
+	resetGame() {
+		this.generateRandomNumber();
+	}
 
-  render() {
-    console.log("State", this.state);
-    return (
-      <div>
-        <p>Random Number: {this.state.randomNumber}</p>
-        <button onClick={this.resetGame.bind(this)} className="btn red darken-2">
-          Reset
-        </button>
-      </div>
-    );
-  }
+	makeGuess(event) {
+		event.preventDefault();
+		console.log("User number:", this.state.userNumber);
+	}
+
+	render() {
+		const { randomNumber, userNumber } = this.state;
+
+		const btnStyle = {
+			margin: "10px 5px"
+		};
+
+		return (
+			<div>
+				<p>Random Number: {randomNumber}</p>
+				<div className="row">
+					<form className="col s6 offset-s3" onSubmit={this.makeGuess.bind(this)}>
+						<div className="row">
+							<div className="input-field">
+								<input
+									className="center-align"
+									onChange={event => {
+										this.setState({ userNumber: event.target.value });
+									}}
+									value={userNumber}
+									type="number"
+									placeholder="Enter a number"
+								/>
+							</div>
+							<div className="row center-align">
+								<button style={btnStyle} className="btn green darken-2">
+									Guess
+								</button>
+
+								<button
+									style={btnStyle}
+									type="button"
+									onClick={this.resetGame.bind(this)}
+									className="btn red darken-2"
+								>
+									Reset
+								</button>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Game;
